fb422caf76115b7df193dc9ba710cc62cfccdeea
Your Name <matrixth@163.com>
2018-05-01 12:32:59 +0800

    third commit

--- src/main/java/com/mmall/dao/PayInfoMapper.java
+++ src/main/java/com/mmall/dao/PayInfoMapper.java
@@ -1,6 +1,8 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.PayInfo;
+import com.mmall.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface PayInfoMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +16,6 @@ public interface PayInfoMapper {
     int updateByPrimaryKeySelective(PayInfo record);
 
     int updateByPrimaryKey(PayInfo record);
+
+
 }
